c5763b23fcc87a872749fd630e4f156dfffe4324
scripters-academy <info@scripters.academy>
2025-03-15 00:10:41 +0300

    add work item add and list views

---
+++ WorkifyApp/Controllers/WorkItemController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WorkifyApp.Controllers
+{
+    public class WorkItemController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
