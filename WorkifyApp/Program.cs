29be02753c8ae1a07b22e7f433f3db26b883aaa1
scripters-academy <info@scripters.academy>
2025-03-17 23:57:18 +0300

    add Application DB Context

--- WorkifyApp/Program.cs
+++ WorkifyApp/Program.cs
@@ -1,9 +1,15 @@
 
 using Microsoft.AspNetCore.Hosting.StaticWebAssets;
+using Microsoft.EntityFrameworkCore;
+using WorkifyApp.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 StaticWebAssetsLoader.UseStaticWebAssets(builder.Environment, builder.Configuration);
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
