f6154c807c8921b70b186033305df2c7d23bedc4
scripters-academy <info@scripters.academy>
2025-03-20 01:12:11 +0300

    add edit feature

--- WorkifyApp/Controllers/WorkItemController.cs
+++ WorkifyApp/Controllers/WorkItemController.cs
@@ -1,16 +1,47 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WorkifyApp.Data;
+using WorkifyApp.Models;
 
 namespace WorkifyApp.Controllers
 {
     public class WorkItemController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public WorkItemController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var workItems = _db.WorkItems.ToList();
+            return View(workItems);
         }
         public IActionResult Add()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult Add(WorkItem workItem)
+        {
+            _db.WorkItems.Add(workItem);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var workItem = _db.WorkItems.Find(id);
+            return View(workItem);
+        }
+        [HttpPost]
+        public IActionResult Edit(WorkItem workItem)
+        {
+            _db.WorkItems.Update(workItem);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
